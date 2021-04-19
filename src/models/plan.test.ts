1195b6be5a63feea42dad08b41e21d412cd91a09
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-04-19 13:58:22 +0100

    test: plan model integration tests

--- src/models/plan.test.ts
+++ src/models/plan.test.ts
@@ -1,23 +1,115 @@
 import { expect } from 'chai'
-import { findAll, findById } from './plan'
+import { findAll, findById, update, deletePlan } from './plan'
+import { db } from '../db'
 
 
 export default function run(): void {
   it('should return a list of plans', async () => {
+    const testAssertions = (r: any) => {
+      expect(r).to.be.an('array')
+      expect(r.length).to.eq(3)
+    }
     const res = await findAll(null)
-    expect(res).to.be.an('array')
-    expect(res.length).to.eq(3)
+    testAssertions(res)
+
+    const trx = await db.transaction()
+    try {
+      const trxRes = await findAll(trx)
+      await trx.commit()
+      testAssertions(trxRes)
+    } catch(err) {
+      throw new Error('unexpected error')
+    }
+  })
+
+  it('should return null when a plan is not found', async () => {
+    const testAssertions = (r: any) => {
+      expect(r).to.eq(null)
+    }
+    const res = await findById(null, 1000)
+    testAssertions(res)
+
+    const trx = await db.transaction()
+    try {
+      const trxRes = await findById(null, 1000)
+      await trx.commit()
+      testAssertions(trxRes)
+    } catch(err) {
+      throw new Error('unexpected error')
+    }
   })
 
   it('should return a plan with id 1', async () => {
-    const res = await findById(null, 1)
-    expect(res).to.be.an('object')
-    expect(res).to.deep.eq({
+    const plan = {
       id: 1,
       name: 'Starter Pack',
       price: '200.00',
       credits: 200,
       periodicity: null,
-    })
+    }
+
+    const testAssertions = (r: any) => {
+      expect(r).to.be.an('object')
+      expect(r).to.deep.eq(plan)
+    }
+
+    const res = await findById(null, 1)
+    testAssertions(res)
+
+    const trx = await db.transaction()
+    try {
+      const trxRes = await findById(null, 1)
+      await trx.commit()
+      testAssertions(trxRes)
+    } catch(err) {
+      throw new Error('unexpected error')
+    }
+  })
+
+  it('should update all properties of a plan', async () => {
+    const newPlanData = {
+      name: 'Edited Starter Pack',
+      price: 900,
+      credits: 900,
+      periodicity: '1 year',
+    }
+
+    const testAssertions = (r: any) => {
+      expect(r).to.be.an('object')
+      expect(r).to.deep.eq({
+        id: 1,
+        ...newPlanData,
+        price: '900.00',
+      })
+    }
+
+    const res = await update(null, 1, newPlanData)
+    testAssertions(res)
+
+    const trx = await db.transaction()
+    try {
+      const trxRes = await update(null, 1, newPlanData)
+      await trx.commit()
+      testAssertions(trxRes)
+    } catch(err) {
+      throw new Error('unexpected error')
+    }
+  })
+
+  it('should delete a plan', async () => {
+    const testAssertions = (r: any) => {
+      expect(r).to.eq(3)
+    }
+    const res = await deletePlan(null, 3)
+    testAssertions(res)
+
+    const trx = await db.transaction()
+    try {
+      const trxRes = await deletePlan(null, 3)
+      await trx.commit()
+      testAssertions(trxRes)
+    } catch(err) {
+      throw new Error('unexpected error')
+    }
   })
 }
